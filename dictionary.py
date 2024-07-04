b1f44f0c4f660fcc1824b6a2b84632108fc014fc
sainadha-reddy3 <158275434+sainadha-reddy3@users.noreply.github.com>
2024-07-04 11:49:46 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+from PyDictionary import PyDictionary
+dictionary = PyDictionary()                   
+while True:
+
+    word = input("Enter your word: ")
+    if word == "":
+        break
+    print(dictionary.meaning(word))
